639839600f67f406edd02364c5176b9e7aa52eb7
Jeronimo Cumming <Jeronimo.cumming@gmail.com>
2024-10-10 15:48:13 -0400

    Initial sharing of project

---
+++ GameInterface.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class GameInterface here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.ArrayList;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+public class GameInterface
+{
+    // instance variables - replace the example below with your own
+    public File Wordset;
+    public Scanner Reader;
+    public ArrayList<String> Words;
+    public String holder;
+    public void GameInterface(){
+        
+        
+    }
+    public void Board(){
+        
+    }
+
+    
+}

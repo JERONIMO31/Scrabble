84fed8bdededc3f64fb5a9d0a3181a788e57193b
Kaitlyn Conron <kaitlynconron@cmail.carleton.ca>
2024-10-16 18:34:57 -0400

    tile.java

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,14 @@
 package src;
+import java.util.*;
 
 public class Player {
+    private String name;
+    private List<Character> myLetters;
+    private int myScore;
+
+
+    public Player(String name) {
+        this.name = name;
+
+    }
 }
---
+++ src/Tile.java
@@ -0,0 +1,52 @@
+package src;
+
+public class Tile {
+    private final char tileChar;
+
+    public Tile(char tile) {
+        this.tileChar = tile;
+    }
+
+    public Tile() {
+        this.tileChar = 0;
+    }
+
+    public int getTileScore(char tile) {
+        if (tile == 'A'||tile == 'E'||tile == 'I'||tile == 'O'||tile == 'U'||tile == 'L'||tile == 'N'||tile == 'S'||tile == 'T'||tile == 'R') {
+            return 1;
+        }
+        else if (tile == 'D'||tile == 'G') {
+            return 2;
+        }
+        else if (tile == 'B'||tile == 'C'||tile == 'M'||tile == 'P') {
+            return 3;
+        }
+        else if (tile == 'F'||tile == 'H'||tile == 'V'||tile == 'W'||tile == 'Y') {
+            return 2;
+        }
+        else if (tile == 'K') {
+            return 5;
+        }
+        else if (tile == 'J'||tile == 'X') {
+            return 8;
+        }
+        else if (tile == 'Q'||tile == 'Z') {
+            return 10;
+        }
+        else {
+            throw new IllegalArgumentException("That is not a valid tile");
+        }
+    }
+
+    public char getTileChar() {
+        return tileChar;
+    }
+
+    public boolean isEmpty() {
+        return tileChar == 0;
+    }
+
+    public boolean equals(Tile tile) {
+        return tile.getTileChar() == tileChar;
+    }
+}

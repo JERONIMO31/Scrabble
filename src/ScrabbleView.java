c5ebbed2a9ed04f23fb94743208eec912d74aacb
Kaitlyn Conron <kaitlynconron@cmail.carleton.ca>
2024-11-05 16:13:41 -0500

    view completion (for now)

--- src/ScrabbleView.java
+++ src/ScrabbleView.java
@@ -1,11 +1,12 @@
-
 import javax.swing.*;
+import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.Iterator;
 import java.util.List;
 import java.awt.*;
 
 public class ScrabbleView extends JFrame {
+    ScrabbleModel model;
     private final JPanel boardPanel;
     private final JButton[][] boardCells;
     private final JPanel playerHandPanel;
@@ -14,15 +15,21 @@ public class ScrabbleView extends JFrame {
     private final JLabel scoreLabel;
     private final JPanel controlPanel;
     private final JButton playWordButton;
-    private final JMenuItem resetGameItem, resetGameSPItem;
+    private final JMenuItem resetGameItem, resetGameSPItem, helpItem;
+    private String scoreStr;
 
 
-    public ScrabbleView(ScrabbleModel model) {
+    public ScrabbleView(ScrabbleModel model) throws FileNotFoundException {
         // Initialize JFrame
+        this.scoreStr = "";
         this.setTitle("Scrabble");
         this.setLayout(new BorderLayout());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500, 500);
+
+        model = new ScrabbleModel(this);
+        
+        ScrabbleController sc = new ScrabbleController(this.model, this);
 
         // Initialize boardPanel
         boardPanel = new JPanel(new GridLayout(15, 15));
@@ -31,6 +38,8 @@ public class ScrabbleView extends JFrame {
             for (int j = 0; j < 15; j++) {
                 JButton boardCell = new JButton();
                 boardCell.setPreferredSize(new Dimension(40, 40));
+                boardCell.setActionCommand("B " + i + " " + j);
+                boardCell.addActionListener(sc);
                 boardCells[i][j] = boardCell;
                 boardPanel.add(boardCell);
             }
@@ -43,29 +52,42 @@ public class ScrabbleView extends JFrame {
         for (int i = 0; i < 7; i++) {
             handTiles[i] = new JButton(" ");
             handTiles[i].setPreferredSize(new Dimension(40, 40));
+            handTiles[i].setActionCommand("H " + i);
+            handTiles[i].addActionListener(sc);
             playerHandPanel.add(handTiles[i]);
         }
 
         // Initialize scorePanel
         scorePanel = new JPanel();
-        String scoreStr = "Scores:\n";
-        for (Player player : model.getAllPlayers()) {
-            scoreStr += player.getName() + ": " + player.getScore() + "\n";
-        }
+        String scoreStr = getScoreString();
         scoreLabel = new JLabel(scoreStr);
         scorePanel.add(scoreLabel);
 
         // Initialize controlPanel
         controlPanel = new JPanel();
         playWordButton = new JButton("Play Word");
+        playWordButton.setActionCommand("P");
+        playWordButton.addActionListener(sc);
         controlPanel.add(playWordButton);
 
         // Add gameMenu and items
         JMenu gameMenu = new JMenu("Game");
         resetGameSPItem = new JMenuItem("Restart game (with same players)");
         resetGameItem = new JMenuItem("New game (with new players)");
+        helpItem = new JMenuItem("Help");
+
+        resetGameSPItem.setActionCommand("HELP");
+        resetGameSPItem.addActionListener(sc);
+
+        resetGameSPItem.setActionCommand("RGSP");
+        resetGameSPItem.addActionListener(sc);
+
+        resetGameItem.setActionCommand("RGNP");
+        resetGameItem.addActionListener(sc);
+
         gameMenu.add(resetGameItem);
         gameMenu.add(resetGameSPItem);
+        gameMenu.add(helpItem);
 
         // Add panels to the frame
         add(boardPanel, BorderLayout.CENTER);
@@ -78,7 +100,16 @@ public class ScrabbleView extends JFrame {
         setVisible(true);
     }
 
+    private String getScoreString() {
+        for (Player player : this.model.getPlayers()) {
+            this.scoreStr = "Scores:\n" + player.getName() + ": " + player.getScore() + "\n";
+        }
+        return scoreStr;
+    }
+
     public void updateBoard() {
+
+
 
 
 
@@ -152,13 +183,28 @@ public class ScrabbleView extends JFrame {
         return resetGameSPItem;
     }
 
+    public JMenuItem getHelpItem() {
+        return helpItem;
+    }
+
     public void highlightHandTile(int tileIndex) {
         for (JButton tile : handTiles) {
-            tile.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+            tile.setBackground(Color.white);
         }
-
         if (tileIndex >= 0 && tileIndex < handTiles.length) {
-            handTiles[tileIndex].setBorder(BorderFactory.createLineBorder(Color.BLUE, 3));
+            handTiles[tileIndex].setBackground(Color.blue);
         }
     }
+
+    public void removedTempTile(int x, int y) {
+        boardCells[x][y].setText(" ");
+        boardCells[x][y].setBackground(Color.white);
+    }
+
+    public void addTempTile(char tile, int x, int y) {
+        boardCells[x][y].setText(String.valueOf(tile));
+        boardCells[x][y].setBackground(Color.gray);
+    }
+
+
 }
